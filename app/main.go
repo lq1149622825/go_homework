8163038f458c88a14fa6dcef9ad1c914c7e6dd58
“qiang.lv” <qiang.lv@thoughtworks.com>
2022-12-15 11:22:43 +0800

    init proxy config

--- app/main.go
+++ app/main.go
@@ -1,14 +1,55 @@
-package app
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
 
-func GetData(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"data": "hello go!",
-	})
+var myProxy = getConfigInfo()
+
+func getConfigInfo() *viper.Viper {
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	config := viper.New()
+	config.AddConfigPath(path)     //设置读取的文件路径
+	config.SetConfigName("config") //设置读取的文件名
+	config.SetConfigType("yaml")   //设置文件的类型
+
+	if err := config.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	return config
+}
+
+func Proxy(c *gin.Context) {
+	remote, err := url.Parse(myProxy.GetString("proxy_server.location.proxy_pass"))
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
 func main() {
 	server := gin.Default()
-	server.GET("/", GetData)
+	server.GET("", Proxy)
+	err := server.Run(myProxy.GetString("proxy_server.listen"))
+	if err != nil {
+		fmt.Println("server failure")
+	}
 }
